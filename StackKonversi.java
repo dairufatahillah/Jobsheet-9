2af295f4c634e839e6cbfbac6d800af5a6501b43
Dairu Fatahillah <dairufatahillah60@gmail.com>
2026-04-22 20:44:13 +0700

    Percobaan 1

---
+++ StackKonversi.java
@@ -0,0 +1,18 @@
+public class StackKonversi {
+
+    public void push(int sisa) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'push'");
+    }
+
+    public boolean isEmpty() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isEmpty'");
+    }
+
+    public String pop() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'pop'");
+    }
+
+}
